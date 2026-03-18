7c9ee68fcf5628fb3fb51a07d5b02238f82079f5
LokiiTechiee <logeslogesh04@gmail.com>
2026-03-19 01:33:10 +0530

    modified webhook.js and index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const webhook = require('./routes/webhook');
 const app = express();
 app.use(express.json());
 
+// Home route
 app.get('/', (req, res) => {
   res.send("Maavu Kadai - DOSA BATTER WhatsApp Bot Running 🚀");
 });
 
-app.use('/webhook', webhook);
+// ✅ FIXED ROUTE
+app.use('/', webhook);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running...");
